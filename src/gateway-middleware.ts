6bead28afe2376500ac78c05bc1b9dfb5fb32ca6
siddhant21101 <siddhant0601@gmail.com>
2024-12-28 17:34:47 +0530

    added helpers

---
+++ src/gateway-middleware.ts
@@ -0,0 +1,20 @@
+import JWT from 'jsonwebtoken';
+import { Request, Response, NextFunction } from 'express';
+import { NotAuthorizedError } from './error-handler';
+const tokens: string[] = ['auth', 'seller', 'gig', 'search', 'buyer', 'message', 'order', 'review'];  //incoming jwt will have id and it will be any of the token
+export function verifyGatewayRequest(req: Request , _res:Response , next: NextFunction) : void {
+    if(!req.headers?.gatewaytoken){
+        throw new NotAuthorizedError("invalid request","verifyGatewayRequest -> gateway-middleware->src->9-ecom-shared");
+    }
+    const token : string = req.headers.gatewaytoken as string ;
+    try{
+        const payload:{id:string , iad:number} =JWT.verify(token , '') as {id:string , iad:number};
+        if (!tokens.includes(payload.id)) {
+            throw new NotAuthorizedError('Invalid request', 'verifyGatewayRequest() method: Request payload is invalid');
+          }
+    }
+    catch (error) {
+        throw new NotAuthorizedError('Invalid request', 'verifyGatewayRequest() method: Request not coming from api gateway');
+    }
+
+}
