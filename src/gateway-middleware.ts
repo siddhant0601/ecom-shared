b91ce14e752e1b17e56f1b8553439c5df88d76c8
siddhant21101 <siddhant0601@gmail.com>
2025-01-04 18:54:01 +0530

    updaed

--- src/gateway-middleware.ts
+++ src/gateway-middleware.ts
@@ -8,7 +8,7 @@ export function verifyGatewayRequest(req: Request , _res:Response , next: NextFu
     }
     const token : string = req.headers.gatewaytoken as string ;
     try{
-        const payload:{id:string , iad:number} =JWT.verify(token , '') as {id:string , iad:number};
+        const payload:{id:string , iad:number} =JWT.verify(token , '1282722b942e08c8a6cb033aa6ce850e') as {id:string , iad:number};
         if (!tokens.includes(payload.id)) {
             throw new NotAuthorizedError('Invalid request', 'verifyGatewayRequest() method: Request payload is invalid');
           }
